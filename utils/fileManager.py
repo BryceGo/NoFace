d562baf6ef0cc62c0ef119013dad4135be752d02
Bryce <brycegolamco@gmail.com>
2020-03-09 15:53:22 -0700

    Changed ffmpeg reading to opencv

--- utils/fileManager.py
+++ utils/fileManager.py
@@ -17,25 +17,33 @@ class fileManager:
             if os.path.exists(filename) != True:
                 raise Exception("Error, file not found!")
 
-            self.input =  (
-                    ffmpeg
-                    .input(filename)
-                    .output('pipe:', format='rawvideo', pix_fmt='rgb24')
-                    .run_async(pipe_stdout=True, cmd=base_path + "ffmpeg")
-                    )
-            probe = ffmpeg.probe(filename, cmd=base_path + "ffprobe")
-            video_info = next(s for s in probe['streams'] if s['codec_type'] == 'video')
-            self.width = int(video_info['width'])
-            self.height = int(video_info['height'])
-            self.num_frames = int(video_info['nb_frames'])
+            self.input = cv2.VideoCapture(filename)
+            self.width  = int(self.input.get(cv2.CAP_PROP_FRAME_WIDTH))
+            self.height = int(self.input.get(cv2.CAP_PROP_FRAME_HEIGHT)) 
+            self.fps = int(self.input.get(cv2.CAP_PROP_FPS)) 
+            self.num_frames = int(self.input.get(cv2.CAP_PROP_FRAME_COUNT))
 
-            try:
-                self.fps = int(Fraction(video_info['avg_frame_rate']))
-            except:
-                # Remove raised error
-                raise Exception("ERROR--- fps")
-                self.fps = 30
-            self.dimensions = 3
+
+
+            # self.input =  (
+            #         ffmpeg
+            #         .input(filename)
+            #         .output('pipe:', format='rawvideo', pix_fmt='rgb24')
+            #         .run_async(pipe_stdout=True, cmd=base_path + "ffmpeg")
+            #         )
+            # probe = ffmpeg.probe(filename, cmd=base_path + "ffprobe")
+            # video_info = next(s for s in probe['streams'] if s['codec_type'] == 'video')
+            # self.width = int(video_info['width'])
+            # self.height = int(video_info['height'])
+            # self.num_frames = int(video_info['nb_frames'])
+
+            # try:
+            #     self.fps = int(Fraction(video_info['avg_frame_rate']))
+            # except:
+            #     # Remove raised error
+            #     raise Exception("ERROR--- fps")
+            #     self.fps = 30
+            # self.dimensions = 3
 
         else:
             self.input = cv2.VideoCapture(0)
@@ -47,7 +55,7 @@ class fileManager:
         if save_file == True:
             self.output = (
                     ffmpeg
-                    .input('pipe:', format='rawvideo', pix_fmt='rgb24', s='{}x{}'.format(self.width, self.height))
+                    .input('pipe:', framerate=self.fps, format='rawvideo', pix_fmt='rgb24', s='{}x{}'.format(self.width, self.height))
                     .output(output_file, pix_fmt='yuv420p')
                     .overwrite_output()
                     .run_async(pipe_stdin=True, cmd=base_path + "ffmpeg")
@@ -68,11 +76,12 @@ class fileManager:
 
     def __del__(self):
         try:
-            if self.from_file == True:
-                self.input.stdout.close()
-                self.input.wait()
-            else:
-                self.input.release()
+            self.input.release()
+            # if self.from_file == True:
+            #     self.input.stdout.close()
+            #     self.input.wait()
+            # else:
+            #     self.input.release()
         except Exception as e:
             raise e
             # TODO: DO nothing
@@ -90,26 +99,26 @@ class fileManager:
         cv2.destroyAllWindows()
 
     def read(self):
-        if self.from_file == True:
-            in_bytes = self.input.stdout.read(self.width*self.height*self.dimensions)
+        return self.input.read()
+        # if self.from_file == True:
+        #     in_bytes = self.input.stdout.read(self.width*self.height*self.dimensions)
 
-            if not in_bytes:
-                return False, np.array([], dtype=np.uint8)
+        #     if not in_bytes:
+        #         return False, np.array([], dtype=np.uint8)
 
-            retVal = (
-                np
-                .frombuffer(in_bytes, np.uint8)
-                .reshape([self.height, self.width, self.dimensions])
-                )
+        #     retVal = (
+        #         np
+        #         .frombuffer(in_bytes, np.uint8)
+        #         .reshape([self.height, self.width, self.dimensions])
+        #         )
             
-            return True, retVal
-        else:
-            return self.input.read()
+        #     return True, retVal
+        # else:
+        #     return self.input.read()
 
     def write(self, frame):
-        print("Wrote frame, ---", frame.shape)
         if self.save_file == True:
-
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             self.output.stdin.write(
                 frame
                 .astype(np.uint8)
