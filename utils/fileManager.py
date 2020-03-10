c97f7a6e0c63d96b9d44752bd5b3ec2cba2178ba
Bryce <brycegolamco@gmail.com>
2020-03-09 19:53:04 -0700

    Remove extra code and placeholder code

--- utils/fileManager.py
+++ utils/fileManager.py
@@ -23,28 +23,6 @@ class fileManager:
             self.fps = int(self.input.get(cv2.CAP_PROP_FPS)) 
             self.num_frames = int(self.input.get(cv2.CAP_PROP_FRAME_COUNT))
 
-
-
-            # self.input =  (
-            #         ffmpeg
-            #         .input(filename)
-            #         .output('pipe:', format='rawvideo', pix_fmt='rgb24')
-            #         .run_async(pipe_stdout=True, cmd=base_path + "ffmpeg")
-            #         )
-            # probe = ffmpeg.probe(filename, cmd=base_path + "ffprobe")
-            # video_info = next(s for s in probe['streams'] if s['codec_type'] == 'video')
-            # self.width = int(video_info['width'])
-            # self.height = int(video_info['height'])
-            # self.num_frames = int(video_info['nb_frames'])
-
-            # try:
-            #     self.fps = int(Fraction(video_info['avg_frame_rate']))
-            # except:
-            #     # Remove raised error
-            #     raise Exception("ERROR--- fps")
-            #     self.fps = 30
-            # self.dimensions = 3
-
         else:
             self.input = cv2.VideoCapture(0)
             self.width  = int(self.input.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -60,9 +38,6 @@ class fileManager:
                     .overwrite_output()
                     .run_async(pipe_stdin=True, cmd=base_path + "ffmpeg")
                     )
-        else:
-            # Nothing needed to construct
-            pass
 
         self.input_file = filename
         self.output_file = output_file
@@ -77,14 +52,7 @@ class fileManager:
     def __del__(self):
         try:
             self.input.release()
-            # if self.from_file == True:
-            #     self.input.stdout.close()
-            #     self.input.wait()
-            # else:
-            #     self.input.release()
         except Exception as e:
-            raise e
-            # TODO: DO nothing
             pass
 
         try:
@@ -92,29 +60,12 @@ class fileManager:
                 self.output.stdin.close()
                 self.output.wait()
         except Exception as e:
-            raise e
-            # TODO: DO nothing
             pass
 
         cv2.destroyAllWindows()
 
     def read(self):
         return self.input.read()
-        # if self.from_file == True:
-        #     in_bytes = self.input.stdout.read(self.width*self.height*self.dimensions)
-
-        #     if not in_bytes:
-        #         return False, np.array([], dtype=np.uint8)
-
-        #     retVal = (
-        #         np
-        #         .frombuffer(in_bytes, np.uint8)
-        #         .reshape([self.height, self.width, self.dimensions])
-        #         )
-            
-        #     return True, retVal
-        # else:
-        #     return self.input.read()
 
     def write(self, frame):
         if self.save_file == True:
